677bbf10d0e6781bfa013afbddcb4e2fc99ae1a5
ServrCo <67353550+ServrCo@users.noreply.github.com>
2021-11-25 10:46:08 -0800

    Just renamed a function.
    	↑	↑	↑

--- Manager/manager.py
+++ Manager/manager.py
@@ -1,6 +1,6 @@
 import subprocess
 import sys
 
-class update(object):
-	def all(self):
+class manager(object):
+	def updateall(self):
 		subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "../requirements.txt"])
